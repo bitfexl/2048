08bd2cc69eed64a526939fed93c303d23e40a235
bitfexl <54662051+bitfexl@users.noreply.github.com>
2023-05-15 16:31:53 +0200

    added server and user.js

---
+++ js/play2048.user.js
@@ -0,0 +1,84 @@
+// ==UserScript==
+// @name         2048 Solver
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Solve 2048.
+// @author       bitfexl
+// @match        https://play2048.co/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=play2048.co
+// @grant        GM_xmlhttpRequest
+// @connect      localhost
+// ==/UserScript==
+
+(async function () {
+    "use strict";
+
+    const oldEmit = KeyboardInputManager.prototype.emit;
+    KeyboardInputManager.prototype.emit = function (e, t) {
+        window.game = this;
+        return oldEmit.apply(this, [e, t]);
+    };
+
+    function parse(gameState) {
+        let bytesSaveStateVertical = [];
+
+        for (let col of gameState.grid.cells) {
+            for (let field of col) {
+                if (field == null) {
+                    bytesSaveStateVertical.push(0);
+                } else {
+                    bytesSaveStateVertical.push(Math.log2(field.value));
+                }
+            }
+        }
+
+        let bytesSaveStateHorizontal = [];
+
+        for (let r = 0; r < 4; r++) {
+            for (let c = 0; c < 4; c++) {
+                bytesSaveStateHorizontal.push(bytesSaveStateVertical[c * 4 + r]);
+            }
+        }
+
+        return bytesSaveStateHorizontal;
+    }
+
+    async function requestMove(boardArr) {
+        return new Promise((resolve) => {
+            GM_xmlhttpRequest({
+                method: "POST",
+                url: "http://localhost:3000/solve",
+                data: JSON.stringify(boardArr),
+                onload(response) {
+                    resolve(response.responseText);
+                },
+            });
+        });
+    }
+
+    setTimeout(restart, 100);
+
+    while (true) {
+        if (localStorage.getItem("gameState") == null) {
+            await new Promise((resolve) => setTimeout(resolve, 2000));
+            restart();
+        }
+
+        let moves = {
+            UP: 0,
+            RIGHT: 1,
+            DOWN: 2,
+            LEFT: 3,
+        };
+
+        let move = await requestMove(parse(JSON.parse(localStorage.getItem("gameState"))));
+
+        console.log(move);
+
+        window.game.emit("move", moves[move]);
+    }
+
+    function restart() {
+        document.querySelector("body > div.container > div.above-game > a").click();
+    }
+})();
---
+++ src/main/java/com/github/bitfexl/_2048/SolverServer.java
@@ -0,0 +1,77 @@
+package com.github.bitfexl._2048;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class SolverServer {
+    private final GameBoardImpl gameBoard = new GameBoardImpl(4);
+    private final GameSolver gameSolver = new GameSolverImpl();
+
+    private final Gson gson = new Gson();
+
+    private final HttpServer httpServer;
+
+    private final int port;
+
+    public SolverServer(int port) throws IOException {
+        this.port = port;
+        this.httpServer = HttpServer.create(new InetSocketAddress(InetAddress.getByAddress(new byte[] { 127, 0, 0, 1 }), port), 10);
+        this.httpServer.createContext("/", this::handleRequest);
+    }
+
+    private void handleRequest(HttpExchange exchange) throws IOException {
+        System.out.println(exchange.getRequestMethod() + "  " + exchange.getRequestURI());
+
+        if (exchange.getRequestURI().toString().equals("/solve")) {
+            byte[] response;
+            int responseCode = 200;
+
+            try {
+                byte[] save = gson.fromJson(new InputStreamReader(exchange.getRequestBody(), StandardCharsets.UTF_8), byte[].class);
+                gameBoard.load(save);
+                final Direction bestMove = gameSolver.bestMove(gameBoard);
+                response = bestMove.toString().getBytes(StandardCharsets.UTF_8);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                response = ex.getMessage().getBytes(StandardCharsets.UTF_8);
+                responseCode = 500;
+            }
+
+            exchange.getResponseHeaders().add("Content-Type", "text/plain; charset=utf-8");
+            exchange.sendResponseHeaders(responseCode, response.length);
+
+            exchange.getResponseBody().write(response);
+        } else {
+            exchange.sendResponseHeaders(404, -1);
+        }
+        exchange.close();
+    }
+
+    public void start() {
+        httpServer.start();
+    }
+
+    public void stop() {
+        httpServer.stop(0);
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public static void main(String[] args) throws IOException {
+        SolverServer server = new SolverServer(3000);
+        server.start();
+        new Scanner(System.in).nextLine();
+        server.stop();
+    }
+}
